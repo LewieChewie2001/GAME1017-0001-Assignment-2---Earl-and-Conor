c7a7f2df85f2f78d964fbc1dd71a623a45afc070
LewieChewie2001 <conordarcy1234@gmail.com>
2025-04-09 21:49:10 -0400

    added music for the losing and start scenes

--- GAME1017_W12_LabDone/Assets/_MyAssets/_Scripts/SceneChanger.cs
+++ GAME1017_W12_LabDone/Assets/_MyAssets/_Scripts/SceneChanger.cs
@@ -5,36 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (Game.Instance == null || Game.Instance.SOMA == null) return;
-
-        switch (scene.name)
-        {
-            case "StartScene":
-                Game.Instance.SOMA.PlayMusic("StartTheme");
-                break;
-            case "LoseScene":
-                Game.Instance.SOMA.PlayMusic("LoseTheme");
-                break;
-            case "GameScene":
-                Game.Instance.SOMA.PlayMusic("I_Ran");
-                break;
-        }
     }
 }
